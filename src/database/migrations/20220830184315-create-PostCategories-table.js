d115b3a1f342e9c24a3ee147514b29c4a0bcccd5
Wellington <wellingtonmachioni@gmail.com>
2022-08-30 15:48:22 -0300

    Criado migration para tabela PostCategories

---
+++ src/database/migrations/20220830184315-create-PostCategories-table.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      `CREATE TABLE PostCategories (
+        postId INT NOT NULL,
+        categoryId INT NOT NULL,
+        PRIMARY KEY (postId, categoryId),
+        CONSTRAINT fk_blogPosts FOREIGN KEY (postId) REFERENCES BlogPosts (id),
+        CONSTRAINT fk_categories FOREIGN KEY (postId) REFERENCES Categories (id)
+      );`
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      `DROP TABLE PostCategories;`
+    );
+  }
+};
