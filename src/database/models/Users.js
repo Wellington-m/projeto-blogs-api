018f44ed2034183e5706d182ff6ff891917c6b69
Wellington <wellingtonmachioni@gmail.com>
2022-08-30 15:05:31 -0300

    Criado model para tabela Users

---
+++ src/database/models/Users.js
@@ -0,0 +1,24 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    displayName: {
+      type:  DataTypes.VARCHAR,
+      allowNull: false,
+    },
+    email: {
+      type:  DataTypes.VARCHAR,
+      allowNull: false,
+    },
+    password: {
+      type:  DataTypes.VARCHAR,
+      allowNull: false,
+    },
+    image: {
+      type:  DataTypes.VARCHAR,
+      allowNull: false,
+    },
+  }, { tableName: 'Users', timestamps: false });
+
+  return Users;
+};
