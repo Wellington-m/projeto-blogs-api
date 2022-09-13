4bd0aca6f41e59eaa64500666ceecf9713c396be
Wellington <wellingtonmachioni@gmail.com>
2022-09-13 18:13:53 -0300

    refactor(rota post): Implementado validações para cadastro de um novo post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,6 +4,7 @@ const createBlogPost = async (req, res) => {
   const { title, content, categoryIds } = req.body;
   const { id: userId } = req;
   const result = await postService.createBlogPost({ title, content, categoryIds, userId });
+  if (!result) return res.status(400).json({ message: '"categoryIds" not found' });
   return res.status(201).json(result);
 };
 
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,8 @@
-const { BlogPost: blogPostModel, PostCategory: PostCategoryModel } = require('../database/models');
+const { 
+  BlogPost: blogPostModel,
+  PostCategory: PostCategoryModel,
+  Category: categoryModel,
+} = require('../database/models');
 
 const findByPk = async (id) => {
   const result = await blogPostModel.findByPk(id);
@@ -7,6 +11,9 @@ const findByPk = async (id) => {
 };
 
 const createBlogPost = async ({ title, content, categoryIds, userId }) => {
+  const categoryIdsPromises = categoryIds.map((id) => categoryModel.findByPk(id));
+  const categories = await Promise.all(categoryIdsPromises);
+  if (categories.some((category) => category === null)) return false;
   const { dataValues } = await blogPostModel.create({ title, content, userId });
   const postCategoryIds = categoryIds.reduce((acc, curr, index) => {
     acc[index] = { postId: dataValues.id, categoryId: curr };
