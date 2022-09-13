75b6b4382cace38c97741ffcbf750eb3f46cbe7f
Wellington <wellingtonmachioni@gmail.com>
2022-09-13 20:42:11 -0300

    feat(rota post): Implementado validações para cadastrar um novo blogPost

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -2,12 +2,13 @@ const express = require('express');
 const tokenValidation = require('../middlewares/tokenValidation');
 const postController = require('../controllers/postController');
 const createPostValidation = require('../middlewares/createPostValidation');
+const updatePostValidation = require('../middlewares/updatePostValidation');
 
 const postRoute = express.Router();
 
 postRoute.get('/:id', tokenValidation, postController.findBlogPostAndCategoryById);
 postRoute.get('/', tokenValidation, postController.findBlogPostsAndCategories);
-postRoute.put('/:id', tokenValidation, postController.update);
+postRoute.put('/:id', tokenValidation, updatePostValidation, postController.update);
 postRoute.post('/', tokenValidation, createPostValidation, postController.createBlogPost);
 
 module.exports = postRoute;
