b5fbbcf6b91e80da501b223b49c14a13ec620030
Wellington <wellingtonmachioni@gmail.com>
2023-05-05 15:35:02 -0300

    feat: Implementado teste da migration para as tabelas BlogPosts e PostCategories

--- tests/integration/migrations.test.js
+++ tests/integration/migrations.test.js
@@ -33,4 +33,37 @@ describe("Cria migrations para as entidades User, Categories, BlogPosts e PostCa
         const [result] = await database.query(queries.select.categories, { type: 'SELECT' });
         expect(result).toEqual(queries.expect.categories);
     });
+
+    it("É possível fazer um INSERT e um SELECT na tabela BlogPosts", async () => {
+        const insertQueryUser = await database.query(queries.insert.user, { type: 'INSERT' });
+        expect(insertQueryUser).toEqual([1, 1]);
+
+        const insertQuery = await database.query(queries.insert.blogPosts, { type: 'INSERT' });
+        expect(insertQuery).toEqual([1, 1]);
+
+        const [result] = await database.query(queries.select.blogPosts, { type: 'SELECT' });
+        expect(result).toEqual(expect.objectContaining(queries.expect.blogPosts.general));
+
+        expect(Date.parse(result.published)).toEqual(queries.expect.blogPosts.published);
+        expect(Date.parse(result.updated)).toEqual(queries.expect.blogPosts.updated);
+
+    });
+
+    it("É possível fazer um INSERT e um SELECT na tabela PostCategories", async () => {
+        const insertQueryUser = await database.query(queries.insert.user, { type: 'INSERT' });
+        expect(insertQueryUser).toEqual([1, 1]);
+
+        const insertQueryCategories = await database.query(queries.insert.categories, { type: 'INSERT' });
+        expect(insertQueryCategories).toEqual([1, 1]);
+
+        const insertQueryBlogPosts = await database.query(queries.insert.blogPosts, { type: 'INSERT' });
+        expect(insertQueryBlogPosts).toEqual([1, 1]);
+
+        const insertQueryPostCategories = await database.query(queries.insert.postCategories, { type: 'INSERT' });
+        expect(insertQueryPostCategories).toEqual([0, 1]);
+
+        const [result] = await database.query(queries.select.postCategories, { type: 'SELECT' });
+        console.log(result);
+        expect(result).toEqual(queries.expect.postCategories);
+    });
 })
