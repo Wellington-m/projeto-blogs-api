47c28010eedf0046af28a1aeb251f842afcbbc84
Wellington <wellingtonmachioni@gmail.com>
2023-05-07 16:56:04 -0300

    feat: Criado roteiro para testar a rota GER user/

--- tests/integration/rotaUserfindAll.test.js
+++ tests/integration/rotaUserfindAll.test.js
@@ -1,3 +1,6 @@
 describe('GET Rota: user/ - Listar todos os usuários', () => {
-    it('')
+    it('Token não foi informado', () => {});
+    it('Token for inválido ou expirado', () => {});
+    it('Token for válido e nenhum usuário é encontrado', () => {});
+    it('Token for válido é retornado os usuários', () => {});
 });
