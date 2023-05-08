3728d326cf2a1f1c2ed0695be1d1c8db937610bf
Wellington <wellingtonmachioni@gmail.com>
2023-05-08 17:12:46 -0300

    test: criado esqueleto do teste para rota POST categories/

---
+++ tests/integration/rotaCategoriesCreate.test.js
@@ -0,0 +1,4 @@
+describe('POST Rota: categories/ - Cadastrar uma categoria', () => {
+    it('Não é possível cadastrar uma categoria sem nome', async () => {});
+    it('É possível cadastrar uma categoria com sucesso', async () => {});
+});
