82f5d30133f3ed5c52f0ac9d59970ad0219259db
Wellington <wellingtonmachioni@gmail.com>
2023-05-08 15:35:26 -0300

    feat: criado teste na rota DELETE user/me - deletar usuário ogado

---
+++ tests/integration/rotaUserDestroy.test.js
@@ -0,0 +1,30 @@
+const frisby = require('frisby');
+const { sequelize: sequelizeCli, apiURL } = require('../helpers/constants');
+const shell = require('shelljs');
+
+describe('DELETE Rota: user/me - Deletar usuário logado', () => {
+
+  beforeAll(() => {
+    shell.exec([
+      sequelizeCli.drop,
+      sequelizeCli.create,
+      sequelizeCli.migrate,
+      sequelizeCli.seed,
+    ].join('&&'), { silent: 'false' })
+  });
+
+    it('É possível deletar o usuário logado', async () => {
+        const { json: { token } } = await frisby.post(`${apiURL}/login`, {
+          email: "lewishamilton@gmail.com",
+          password: "123456"
+        }).expect('status', 200);
+        
+        const { body } = await frisby.setup({
+          request: {
+              headers: {
+                  'Authorization': token,
+              }
+          }
+        }).del(`${apiURL}/user/me`).expect('status', 204);
+    });
+});
