0e887d53766e39b5dbbc7c03d8782babb4236193
Wellington <wellingtonmachioni@gmail.com>
2023-05-09 09:05:01 -0300

    test: criado testes para a rota POST categories/

--- tests/integration/rotaCategoriesCreate.test.js
+++ tests/integration/rotaCategoriesCreate.test.js
@@ -1,4 +1,54 @@
+const frisby = require('frisby');
+const { sequelize: sequelizeCli, apiURL } = require('../helpers/constants');
+const shell = require('shelljs');
+
 describe('POST Rota: categories/ - Cadastrar uma categoria', () => {
-    it('Não é possível cadastrar uma categoria sem nome', async () => {});
-    it('É possível cadastrar uma categoria com sucesso', async () => {});
+    beforeAll(() => {
+        shell.exec([
+            sequelizeCli.drop,
+            sequelizeCli.create,
+            sequelizeCli.migrate,
+            sequelizeCli.seed
+        ].join('&&'), {
+            silent: 'false',
+        })
+    });
+
+    it('Não é possível cadastrar uma categoria sem nome', async () => {
+        const { json: { token } } = await frisby.post(`${apiURL}/login`, {
+            email: "lewishamilton@gmail.com",
+            password: "123456",
+        }).expect('status', 200);
+        
+        const { body } = await frisby.setup({
+            request: {
+                headers: {
+                    'Authorization': token,
+                }
+            }
+        }).post(`${apiURL}/categories`).expect('status', 400);
+
+        const result = JSON.parse(body);
+        expect(result.message).toBe('"name" is required');
+    });
+    it('É possível cadastrar uma categoria com sucesso', async () => {
+        const { json: { token } } = await frisby.post(`${apiURL}/login`, {
+            email: "lewishamilton@gmail.com",
+            password: "123456",
+        }).expect('status', 200);
+
+        const { body } = await frisby.setup({
+            request: {
+                headers: {
+                    'Authorization': token,
+                }
+            }
+        }).post(`${apiURL}/categories`, {
+            name: 'Teste'
+        }).expect('status', 201);
+
+        const result = JSON.parse(body);
+        expect(result).toHaveProperty('id');
+        expect(result).toHaveProperty('name');
+    });
 });
