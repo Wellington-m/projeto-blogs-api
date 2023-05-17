f2e7146d617768721b587c76b8bfbdcbc34f22a0
Wellington <wellingtonmachioni@gmail.com>
2023-05-17 15:31:56 -0300

    test: criado esqueleto do teste da rota PUT /post/is

---
+++ tests/integration/postRoute/rotaPostUpdate.test.js
@@ -0,0 +1,6 @@
+describe('PUT Rota: /post/:id - Update a post', () => {
+    it('isnt possible update a post without inform title', () => {});
+    it('isnt possible update a post without inform content', () => {});
+    it('isnt possible update a post from other user', () => {});
+    it('is possible update a post', () => {});
+});
