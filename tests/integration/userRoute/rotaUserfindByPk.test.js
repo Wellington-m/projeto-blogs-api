5b50fee4d17f64f06a8a30e7a742aef2024896e0
Wellington <wellingtonmachioni@gmail.com>
2023-05-24 17:31:43 -0300

    test: refatorado teste rota GET user/:id

--- tests/integration/userRoute/rotaUserfindByPk.test.js
+++ tests/integration/userRoute/rotaUserfindByPk.test.js
@@ -1,111 +1,65 @@
-const frisby = require('frisby');
+const request = require('supertest');
 const shell = require('shelljs');
-// const userService = require('../../src/services/userService');
-const { User: userModel } = require('../../../src/database/models');
-const { sequelize: sequelizeCli, apiURL } = require('../../helpers/constants');
+const api = require('../../../src/api');
+const { registerUser } = require('../../helpers/registerData');
+const { sequelize: sequelizeCli } = require('../../helpers/constants');
 
-describe.skip('GET Rota: user/:id - procurar usuário pelo ID', () => {
-    beforeAll(() => {
-        shell.exec([
-            sequelizeCli.pretest,
-            sequelizeCli.drop,
-            sequelizeCli.create,
-            sequelizeCli.migrate,
-            sequelizeCli.seed
-        ].join(' && '),
-          { silent: process.env.DEBUG === "false" });
+describe('GET Route: user/:id - search user by ID', () => {
+  beforeAll(() => {
+    shell.exec(sequelizeCli.beforetest, {
+      silent: process.env.DEBUG === 'false',
+    });
+  });
+
+  afterAll(() => {
+    shell.exec(sequelizeCli.posttest, { silent: false });
+  });
+
+  it('Is not possible access user route without a token', async () => {
+    const response = await request(api).get('/user/1');
+    expect(response.body.message).toBe('Token not found');
+  });
+
+  it('Is not possible access user route with invalid or expired Token', async () => {
+    const response = await request(api)
+      .get('/user')
+      .set('Authorization', 'invalidToken');
+
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe('Expired or invalid token');
+  });
+
+  it('Is possible find a user by ID', async () => {
+    await registerUser();
+
+    const {
+      body: { token },
+    } = await request(api).post('/login').send({
+      email: 'brett@email.com',
+      password: '123456',
     });
 
-    it('Não é possível acessar a rota User sem um token', async () => {
-        const { body } = await frisby
-        .setup({
-          request: {
-            headers: {
-              'Authorization': ''
-            }
-          }
-        })
-        .get(`${apiURL}/user/1`)
-        .expect('status', 401);
+    const response = await request(api)
+      .get('/user/1')
+      .set('Authorization', token);
 
-        const result = JSON.parse(body);
-        expect(result.message).toBe('Token not found');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('id', 'displayName', 'email', 'image');
+  });
+
+  it('Is displayed the correct message if user does not exist', async () => {
+    const {
+      body: { token },
+    } = await request(api).post('/login').send({
+      email: 'brett@email.com',
+      password: '123456',
     });
 
-    it('Não é possível acessar a rota User com Token inválido ou expirado', async () => {
-        const { body } = await frisby
-        .setup({
-            request: {
-                headers: {
-                    'Authorization': 'tokenInvalido'
-                }
-            }
-        })
-        .get(`${apiURL}/user/1`)
-        .expect('status', 401);
+    const response = await request(api)
+      .get('/user/10')
+      .set('Authorization', token);
 
-        const result = JSON.parse(body);
-        expect(result.message).toBe('Expired or invalid token');
-    });
-
-    it('É possível encontrar um usuário pelo ID', async () => {
-        const { json: { token } } = await frisby.post(`${apiURL}/login`, { 
-            email: 'lewishamilton@gmail.com',
-            password: '123456',
-        }).expect('status', 200);
-
-        const { body } = await frisby
-        .setup({
-            request: {
-                headers: {
-                    'Authorization': token
-                }
-            }
-        })
-        .get(`${apiURL}/user/1`)
-        .expect('status', 200);
-
-        const result = JSON.parse(body);
-        expect(result).toHaveProperty('id', 'displayName', 'email', 'image');
-    });
-
-    it('É exibida a mensagem correta caso o usuário não exista', async () => {
-        const { json: { token } } = await frisby.post(`${apiURL}/login`, { 
-            email: 'lewishamilton@gmail.com',
-            password: '123456',
-        }).expect('status', 200);
-
-        const { body } = await frisby
-        .setup({
-            request: {
-                headers: {
-                    'Authorization': token
-                }
-            }
-        })
-        .get(`${apiURL}/user/10`)
-        .expect('status', 404);
-
-        const result = JSON.parse(body);
-        expect(result.message).toBe('User does not exist');
-    });
-    // it('É exibida a mensagem correta em caso de erro da aplicação', async () => {
-    //     jest.spyOn(userModel, 'findByPk').mockImplementationOnce(() => Promise.reject('fail'));
-
-    //     const { json: { token } } = await frisby.post(`${apiURL}/login`, { 
-    //         email: 'lewishamilton@gmail.com',
-    //         password: '123456',
-    //     }).expect('status', 200);
-
-    //     const { body } = await frisby
-    //     .setup({
-    //         request: {
-    //             headers: {
-    //                 'Authorization': token
-    //             }
-    //         }
-    //     })
-    //     .get(`${apiURL}/user/1`)
-    //     .expect('status', 500);
-    // });
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('User does not exist');
+  });
 });
