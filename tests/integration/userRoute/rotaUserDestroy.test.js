754d8f46bed1addf463fe74aa5055803a2aa1d33
Wellington <wellingtonmachioni@gmail.com>
2023-05-24 16:54:12 -0300

    test: refatorado teste rota DEL /user/me

--- tests/integration/userRoute/rotaUserDestroy.test.js
+++ tests/integration/userRoute/rotaUserDestroy.test.js
@@ -1,31 +1,31 @@
-const frisby = require('frisby');
-const { sequelize: sequelizeCli, apiURL } = require('../../helpers/constants');
+const request = require('supertest');
+const api = require('../../../src/api');
+const { registerUser } = require('../../helpers/registerData');
+const { sequelize: sequelizeCli } = require('../../helpers/constants');
 const shell = require('shelljs');
 
-describe.skip('DELETE Rota: user/me - Deletar usuário logado', () => {
-
-  beforeAll(() => {
-    shell.exec([
-        sequelizeCli.pretest,
-        sequelizeCli.drop,
-        sequelizeCli.create,
-        sequelizeCli.migrate,
-        sequelizeCli.seed
-    ].join('&&'), { silent: 'false' })
+describe('DELETE Rota: user/me - Deletar usuário logado', () => {
+  beforeAll(async () => {
+    shell.exec(sequelizeCli.beforetest, { silent: 'false' });
+    await registerUser();
   });
 
-    it('É possível deletar o usuário logado', async () => {
-        const { json: { token } } = await frisby.post(`${apiURL}/login`, {
-          email: "lewishamilton@gmail.com",
-          password: "123456"
-        }).expect('status', 200);
-        
-        const { body } = await frisby.setup({
-          request: {
-              headers: {
-                  'Authorization': token,
-              }
-          }
-        }).del(`${apiURL}/user/me`).expect('status', 204);
+  afterAll(() => {
+    shell.exec(sequelizeCli.posttest, { silent: false });
+  });
+
+  it('Is possible to delete the logged in user', async () => {
+    const {
+      body: { token },
+    } = await request(api).post('/login').send({
+      email: 'brett@email.com',
+      password: '123456',
     });
+
+    const response = await request(api)
+      .del('/user/me')
+      .set('Authorization', token);
+
+    expect(response.status).toBe(204);
+  });
 });
